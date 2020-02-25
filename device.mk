4e3f46de3780e827bbdb1807ca4899089163d5c5
srhughes <46631343+SafCloud@users.noreply.github.com>
2020-02-25 09:04:17 -0500

    Update device.mk

--- device.mk
+++ device.mk
@@ -27,9 +27,3 @@ DEVICE_PACKAGE_OVERLAYS += device/samsung/serranoltexx/overlay
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.gsm.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.gsm.xml
-
-# FM radio
-#PRODUCT_PACKAGES += \
-#    android.hardware.broadcastradio@1.0-impl \
-#    FM2 \
-#    qcom.fmradio
